7073f898fd25161b65e7211d4954caab1ab1b668
Joe Fleming <joe.fleming@gmail.com>
2017-10-04 12:29:13 -0700

    feat: work with plot data, and series style

--- public/functions/lights/index.js
+++ public/functions/lights/index.js
@@ -6,41 +6,33 @@ import { connect, getUpdater } from '../../lib/port_controller';
 export default new Fn({
   name: 'lights',
   type: 'render',
-  args: {
-    palette: {
-      types: ['palette', 'null'],
-      help: 'A palette object for describing the colors to use on this plot',
-      default: '{palette}',
-    },
-  },
-  fn(context, args) {
+  fn(context) {
     const ledCount = 8;
-    const { palette } = args;
 
-    // calculate the colors to use for each series
-    const colors = palette.gradient
-      ? chroma.scale(palette.colors).colors(context.rows.length)
-      : palette.colors;
+    // get the colors from the plot context
+    const { data, options } = context.value;
+    const records = data.map(rec => ({
+      name: rec.label,
+      color: rec.color,
+      size: rec.data[0][2].size,
+    }));
 
     // find the max brightness, to scale all values to a percentage
-    const maxVal = context.rows.reduce((range, row) => {
-      if (!range) return row.size;
-      return Math.max(range, row.size);
+    const maxVal = records.reduce((range, rec) => {
+      if (!range) return rec.size;
+      return Math.max(range, rec.size);
     }, -Infinity);
 
     // turn each row into an object describing how to light up each LED
-    const values = context.rows.map((row, i) => {
-      // TODO this only supports value count of palette color count * 2
-      const count = context.rows.length;
-      const colorIndex = (i < count) ? i : i - count;
-      const color = colors[colorIndex];
+    const values = records.map((rec, i) => {
+      const color = rec.color || options.colors[i];
 
       if (!color || i >= ledCount) return null;
 
       return {
         id: i,
-        name: row.color,
-        brightness: Math.round(row.size / maxVal * 100) / 100,
+        name: rec.name,
+        brightness: Math.round(rec.size / maxVal * 100) / 100,
         color,
       };
     }).filter(Boolean);
@@ -48,34 +40,16 @@ export default new Fn({
     return connect()
     .then(port => getUpdater(port))
     .then((setLights) => {
-      const summary = values.map((val, i) => {
+      values.forEach((val, i) => {
         // send color data to LEDs
-        setLights(i, chroma(val.color).darken(1 - (val.brightness / 2)).rgb());
-
-        // create LED info string
-        return `${val.name}: ${val.color} (${val.brightness * 100}%)`;
+        setLights(i, chroma(val.color).darken(2 - (val.brightness / 2)).rgb());
       });
 
-      return {
-        type: 'render',
-        as: 'markdown',
-        value: {
-          content: `Check your LED strip
-
-- ${summary.join('\n- ')}`,
-        }
-      };
+      return context;
     })
     .catch((err) => {
-      return {
-        type: 'render',
-        as: 'markdown',
-        value: {
-          content: `Connection failed :(
-
-${err.message}`,
-        }
-      };
+      console.error(err);
+      return context;
     });
   }
 });
