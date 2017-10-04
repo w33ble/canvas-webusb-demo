6930d16240d00f0d74eda04edaadd3138d4c513c
Joe Fleming <joe.fleming@gmail.com>
2017-10-04 12:37:40 -0700

    fix: remove palette from expression type

--- public/expression_types/lights.js
+++ public/expression_types/lights.js
@@ -1,14 +1,8 @@
-import { Arg } from 'plugins/canvas/expression_types/arg';
 import { View } from 'plugins/canvas/expression_types/view';
 
 export default new View('lights', {
   displayName: 'Lights',
   description: 'Show your data on LEDs',
   modelArgs: ['color', 'size'],
-  args: [
-    new Arg('palette', {
-      displayName: 'Color palette',
-      argType: 'palette',
-    }),
-  ],
+  args: [],
 });
